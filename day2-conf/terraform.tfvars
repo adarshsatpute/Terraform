0146b6750eddb7b7e381a6d3e6a1844475de1f82
adarsh <adarshsatpute99@gmail.com>
2025-11-04 18:36:48 +0530

    2nd day update

---
+++ day2-conf/terraform.tfvars
@@ -0,0 +1,2 @@
+ami_id = "ami-0bdd88bd06d16ba03"
+typ = "t3.micro"
---
+++ day2-conf/variable.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {
+    description = "ami-0bdd88bd06d16ba03"
+    type = string
+  
+}
+variable "typ" {
+    description = "t3.micro"
+    type = string
+  
+}
