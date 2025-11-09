563cce2bb06d341fdefd006874c6f9fc863c1e7d
adarsh <adarshsatpute99@gmail.com>
2025-11-09 13:05:37 +0530

    delete my day2 folder

--- day2-conf/output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.name.id
-}
-
-output "public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.name.public_ip
-}
--- day2-conf/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-ami_id = "ami-0bdd88bd06d16ba03"
-typ = "t3.micro"
